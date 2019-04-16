b33521f06d368e6ab75ba70f7d9bec82bfc65f2e
Akbar Rafsanjani <zenproxx@outlook.com>
2019-04-16 14:27:02 +0700

    tampilan kasir sederhana

---
+++ application/khz_module/kasir/views/kasir_v2.php
@@ -0,0 +1,21 @@
+<div class="container-fluid">
+
+	<!-- tampil produk &amp; meja -->
+	<div class="row">
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+	</div>
+
+	<!-- meja yg berisi &amp; pesanan yg terpilih -->
+	<div class="row">
+		<div class="col"></div>
+		<div class="col"></div>
+	</div>
+
+	<!-- tombol action -->
+	<div class="row">
+		<div class="col"></div>
+	</div>
+	
+</div>
